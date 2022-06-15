5fdb60bd2eeb582acc84fec8cd368d9bf98164b4
Bagus Rinaldhi <bagus.rinaldhi@pawoon.com>
2022-06-15 18:49:01 +0700

    push func ganjilgenap

---
+++ ganjilgenap.go
@@ -0,0 +1,11 @@
+package edspert_go1_lib
+
+func cekGanjilGenap(num int) string {
+
+	if num%2 == 0 {
+		return "genap"
+	}else{
+		return "ganjil"
+	}
+
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/bagusrin/edspert-go1-lib
+
+go 1.18
